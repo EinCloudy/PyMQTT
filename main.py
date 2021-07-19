04d5e57f90b73c97756a4280d05b56d0178fa81a
Joel Obi <57414887+cloudlesstv@users.noreply.github.com>
2021-07-19 17:51:34 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import paho.mqtt.client as mqtt
+
+
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Connected successfully")
+    else:
+        print("Connect returned result code: " + str(rc))
+
+
+def on_message(client, userdata, msg):
+    print("Received message: " + msg.topic + " -> " + msg.payload.decode("utf-8"))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+# client.tls_set(tls_version=mqtt.ssl.PROTOCOL_TLS)
+
+client.username_pw_set("username", "password")
+
+client.connect("broker.hivemq.com", 1883)
+
+client.subscribe("home/topic1")
+
+client.loop_forever()
+
+client.publish("home/topic1/switch", "On")
